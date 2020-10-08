9dbfcb5edd72a8a9a9d2b89ec841c5b9291b9f54
André Oliveira <54690333+AndreMents@users.noreply.github.com>
2020-10-08 10:11:26 -0300

    Add files via upload

---
+++ lib/database/Connection.php
@@ -0,0 +1,27 @@
+<?php
+
+abstract class Connection {
+    private static $conn;
+    public static function getConn(){
+        if (self::$conn == null) {
+            self::$conn = new PDO('mysql: host=localhost; dbname=bdpostagem; charset=utf8', 'root', '');
+        }
+
+        return self::$conn;
+    }
+}
+
+
+/*abstract class Connection {
+    private static $conn;
+    public static function getConn(){
+        if (self::$conn = null) {
+            self::$conn == new PDO('mysql: host=localhost; dbname=bdpostagem; charset=utf8', 'root', '');
+        }
+
+        return self::$conn;
+    }
+}
+*/
+
+?>
